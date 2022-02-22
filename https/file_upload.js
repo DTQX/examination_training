67dba032ff9401aa4f959d59a10856c9e812244f
dtq <978097431@qq.com>
2022-02-23 00:43:21 +0800

    feat: add file mock

---
+++ https/file_upload.js
@@ -0,0 +1,19 @@
+// formdata multipart
+const params = {
+    files:{
+        myFile:{
+            name:'xx',
+            buffer: xxx,
+            type,
+        }
+    }
+}
+
+const res =  {
+    code: 0,
+    msg: '上传成功',
+    data: {
+      url: url
+    }
+  };
+};
